d8c314c361bd31f202ab16252db7e9d9811fa9dd
Mikael Midtgaard <mikaelmidt@gmail.com>
2023-12-15 12:29:42 +0100

    use next-sanity util for body parsing

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,32 +1,27 @@
-import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
+import { parseBody } from "next-sanity/webhook";
 import { revalidatePath } from "next/cache";
+import { NextRequest } from "next/server";
 
 const secret = process.env.SANITY_WEBHOOK_SECRET;
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     if (req.method !== "POST") {
       return new Response("Method Not Allowed", { status: 401 });
     }
-    const signature = req.headers[SIGNATURE_HEADER_NAME] as string;
-    const jsonBody = await req.json();
-    const isValid = await isValidSignature(jsonBody, signature, secret);
-    if (!isValid) {
+    const { isValidSignature, body } = await parseBody<{
+      _type: string;
+      slug: { current: string };
+    }>(req, secret);
+
+    if (!isValidSignature) {
       return new Response("Invalid signature", { status: 401 });
     }
 
-    const pathToRevalidate = `/posts/${jsonBody.slug.current}`;
+    const pathToRevalidate = `/posts/${body.slug.current}`;
     revalidatePath(pathToRevalidate);
     return new Response("Success!", { status: 200 });
   } catch (e) {
     return new Response(`Error revalidating: ${e.message}`, { status: 400 });
   }
-}
-
-async function readBody(readable: any) {
-  const chunks = [];
-  for await (const chunk of readable) {
-    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
-  }
-  return Buffer.concat(chunks).toString("utf8");
 }
